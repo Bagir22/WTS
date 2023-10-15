4548aa2542a6a22cc8b294bb765c0d00f13e86d1
Danila Yanvarev <bagir2202@gmail.com>
2023-10-15 12:40:24 +0300

    generate models

---
+++ blog/backend/models/AccessToken.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "accessToken".
+ *
+ * @property int $userId
+ * @property string $accessToken
+ *
+ * @property User $user
+ */
+class AccessToken extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'accessToken';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['accessToken'], 'required'],
+            [['accessToken'], 'string', 'max' => 255],
+            [['accessToken'], 'unique'],
+            [['userId'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['userId' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'userId' => 'User ID',
+            'accessToken' => 'Access Token',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'userId']);
+    }
+}
---
+++ blog/backend/models/Article.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property int|null $userId
+ * @property string $title
+ * @property string $body
+ *
+ * @property User $user
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['userId'], 'integer'],
+            [['title', 'body'], 'required'],
+            [['body'], 'string'],
+            [['title'], 'string', 'max' => 255],
+            [['userId'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['userId' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'userId' => 'User ID',
+            'title' => 'Title',
+            'body' => 'Body',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'userId']);
+    }
+}
